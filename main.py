c65a19eea63fb09e2907140afc65f11343b1db41
alexcodes47 <alex@ipgsd.com>
2025-08-14 12:10:16 -0700

    Asteroid com3

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -18,17 +20,38 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
         updatable.update(dt)
+        for item in asteroids:
+            collide = item.collide(player)
+            if collide == True:
+                print("Game over!")
+                sys.exit()
+        for item in asteroids:
+            for shot in shots:
+                collide = item.collide(shot)
+                if collide == True:
+                    shot.kill()
+                    result = item.split()
+                    if result:
+                        new_asteroid1, new_asteroid2 = result
+                        asteroids.add(new_asteroid1)
+                        asteroids.add(new_asteroid2)
+                    else:
+                        pass
+                    
         #player.draw(screen)
         for item in drawable:
             item.draw(screen)
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -33,7 +35,21 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        
+        self.shot_timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shot_timer > 0:
+            pass
+        else:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+            velocity *= PLAYER_SHOOT_SPEED
+            shot.velocity = velocity
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN
